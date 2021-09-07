ca0ef1f5b6ba94e5901b1c3b5e1759ee836836ff
Corrie <corriestoddard@gmail.com>
2021-09-06 18:38:47 -0700

    Add components

---
+++ src/Components/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../Styles/App.css";
+
+// components
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+const Product = () => {
+  return <div className="App"></div>;
+};
+
+export default Product;
